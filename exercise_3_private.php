a171734241c7116da339e0f833b13b73bdf0a788
YasserB94 <yasserbal@icloud.com>
2022-06-23 15:34:43 +0200

    Exercise completed

--- exercise_3_private.php
+++ exercise_3_private.php
@@ -16,3 +16,20 @@ TODO: Print this method on the screen on a new line.
 
 USE TYPEHINTING EVERYWHERE!
 */
+include_once __DIR__ . '/classes/Beverage.class.php';
+$duvel = new Beer('blond', 3.5, 'Duvel', 8.5);
+//The other way hurts me! - There is only one way
+print_r('AlcoholPercentage: ' . $duvel->getAlcoholPercentage() . "%");
+echo '<br/>';
+print_r('color: ' . $duvel->getColor());
+echo '<br/>';
+print_r(
+    'Info:' . $duvel->getInfo()
+);
+echo '<br/>';
+echo '<br/>';
+$duvel->setColor('light');
+print_r($duvel->getColor());
+echo '<br/>';
+echo '<br/>';
+print_r($duvel->beerInfo());
