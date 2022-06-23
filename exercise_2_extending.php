be321ec0eb75625a251842a4b9c6de1e0eddac9a
YasserB94 <yasserbal@icloud.com>
2022-06-23 14:51:44 +0200

    Exercise completed

--- exercise_2_extending.php
+++ exercise_2_extending.php
@@ -18,3 +18,13 @@ Make sure that each print is on a different line.
 Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
 USE TYPEHINTING EVERYWHERE!
 */
+include_once __DIR__ . '/classes/Beverage.class.php';
+$duvel = new Beer('blond', 3.5, 'Duvel', 8.5);
+//The other way hurts me! - There is only one way
+print_r('AlcoholPercentage: ' . $duvel->getAlcoholPercentage() . "%");
+echo '<br/>';
+print_r('color: ' . $duvel->getColor());
+echo '<br/>';
+print_r(
+    'Info:' . $duvel->getInfo()
+);
