f8d7e88051c2c4c73c0dedcf83194123cd5d1bd4
Yasser Bal <yasserbal@Yassers-Mac-Studio.local>
2022-06-24 10:27:41 +0200

    Finished Ex5

--- exercise_5_public.php
+++ exercise_5_public.php
@@ -10,3 +10,12 @@ TODO: Change the properties to private.
 TODO: Fix the errors without using getter and setter functions.
 TODO: Change the price to 3.5 euro and print it also on the screen on a new line.
 */
+include_once __DIR__ . '/classes/Beverage.class.php';
+
+//The other way hurts me! - There is only one way
+$cola = new Beverage('black', 2);
+echo $cola->getInfo();
+echo '<br/>';
+echo '<br/>';
+$cola->setPrice(3.5);
+echo $cola->getPrice();
